9014ac13b3319904e282cb63e59330c383f25de1
Zoe Frongillo <z.frongillo@ufl.edu>
2019-12-11 17:01:28 -0500

    commit 7

--- app.py
+++ app.py
@@ -21,29 +21,33 @@ itemsSightings = cursorObj.fetchall()
 # close the connection to the database
 conn.close()
 
-
+# This is the app route for the "Flower" Page form to receive user input
 @app.route('/select_flower', methods = ['POST'])
 def select_flower():
+    # Request the receive input (POST) to get the selected flower
     sel_Flower = request.form['sel_Flower']
     print("The selected Flower is '" + sel_Flower + "'")
 
-    # establish a connection with the database file
+    # Establish a connection with the database file
     conn = sqlite3.connect('flowers2019.db')
-    # create a cursor to query the database; does NOT change
+    # create a cursor to query the database within the app route
     cursorObj = conn.cursor()
-    # All items retrieval from SIGHTINGS table in DB
+    # Run the Query on SIGHTINGS table to get the 10 most recent sightings of the selected flower (from user input)
     cursorObj.execute("SELECT * FROM SIGHTINGS WHERE name = \"" + sel_Flower + "\" ORDER by date(sighted) DESC limit 10")
-    # Run Query
-    # save fetch all to itemsSightings list; to be referenced in templates
+    # save the queried tuples to the "flowerMostRec"
     flowerMostRec = cursorObj.fetchall()
 
-    return render_template('flowers_display10.html', flowerMostRec=flowerMostRec) # used to be redirect('/flowers_display10')
+    # close the connection to the database
 
+    # render the next template to display the data
+    return render_template('flowers_display10.html', flowerMostRec=flowerMostRec)
 
+# App route for the page of Flowers on the web app
 @app.route('/flowers')
 def flowers():
     return render_template('flowers.html', itemsFlowers=itemsFlowers)
 
+# App route for the Home page on the web app
 @app.route('/')
 def index():
     return render_template('index.html')
