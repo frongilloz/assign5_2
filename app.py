dec64508cb990a221797fbb03399d5c405b15cff
Jhavaa <Jramisetty@ufl.edu>
2019-12-12 22:00:18 -0500

    Added logging for all tables (INSERT, DELETE, and UPDATE)

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-# Title: Assignment 5
-# Description: A GUI for querying and modifying the flowers2019.db through sqlite3 queries, insertions, and triggers
-# Date: 12/12/2019
-# Authors: Zoe Frongillo and Jhanava Ramisetty
-
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
 app = Flask(__name__)
@@ -23,6 +18,105 @@ cursorObj = conn.execute("SELECT * FROM SIGHTINGS")
 # save fetch all to itemsSightings list; to be referenced in templates
 itemsSightings = cursorObj.fetchall()
 
+cursorObj.execute(
+    "SELECT name FROM sqlite_master WHERE type = 'table' AND name = 'sightings_log';"
+)
+if cursorObj.rowcount == 0:
+    cursorObj.execute(
+        "CREATE TABLE sightings_logs (old_name, new_name, old_person, new_person, old_location, new_location, old_sighted, new_sighted, user_action, created_at);")
+
+cursorObj.execute(
+    "SELECT name FROM sqlite_master WHERE type = 'table' AND name = 'features_log';"
+)
+if cursorObj.rowcount == 0:
+    cursorObj.execute(
+        "CREATE TABLE features_logs (old_location, new_location, old_class, new_class, old_latitude, new_latitude, old_longitude, new_longitude, old_map, new_map, old_elev, new_elev, user_action, created_at);")
+
+cursorObj.execute(
+    "SELECT name FROM sqlite_master WHERE type = 'table' AND name = 'flowers_log';"
+)
+if cursorObj.rowcount == 0:
+    cursorObj.execute(
+        "CREATE TABLE flowers_logs (old_genus, new_genus, old_species, new_species, old_comname, new_comname, user_action, created_at);")
+
+conn.commit()
+
+# Create trigger to log SIGHTINGS, FEATURES, and FLOWERS table insertions, deletions, and updates
+# SIGHTINGS
+cursorObj.execute(
+    "SELECT name FROM sqlite_master WHERE type = 'trigger' AND name = 'sightings_insert_log';"
+)
+if cursorObj.rowcount == 0:
+    cursorObj.execute(
+        "CREATE TRIGGER sightings_insert_log AFTER INSERT ON SIGHTINGS BEGIN INSERT INTO sightings_logs(new_name, new_person, new_location, new_sighted, user_action, created_at) VALUES(new.name, new.person, new.location, new.sighted, 'INSERT', DATETIME('NOW')); END;")
+
+
+cursorObj.execute(
+    "SELECT name FROM sqlite_master WHERE type = 'trigger' AND name = 'sightings_delete_log';"
+)
+if cursorObj.rowcount == 0:
+    cursorObj.execute(
+        "CREATE TRIGGER sightings_delete_log AFTER DELETE ON SIGHTINGS BEGIN INSERT INTO sightings_logs(old_name, old_person, old_location, old_sighted, user_action, created_at) VALUES(old.name, old.person, old.location, old.sighted, 'DELETE', DATETIME('NOW')); END;")
+
+
+cursorObj.execute(
+    "SELECT name FROM sqlite_master WHERE type = 'trigger' AND name = 'sightings_update_log';"
+)
+if cursorObj.rowcount == 0:
+    cursorObj.execute(
+        "CREATE TRIGGER sightings_update_log AFTER UPDATE ON SIGHTINGS WHEN old.name <> new.name OR old.person <> old.person OR old.location <> new.location OR old.sighted <> new.sighted BEGIN INSERT INTO sightings_logs VALUES(old.name, new.name, old.person, new.person, old.location, new.location, old.sighted, new.sighted, 'UPDATE', DATETIME('NOW')); END;")
+
+
+# FEATURES
+cursorObj.execute(
+    "SELECT name FROM sqlite_master WHERE type = 'trigger' AND name = 'features_insert_log';"
+)
+if cursorObj.rowcount == 0:
+    cursorObj.execute(
+        "CREATE TRIGGER features_insert_log AFTER INSERT ON FEATURES BEGIN INSERT INTO features_logs(new_location, new_class, new_latitude, new_longitude, new_map, new_elev, user_action, created_at) VALUES(new.location, new.class, new.latitude, new.longitude, new.map, new.elev, 'INSERT', DATETIME('NOW')); END;")
+
+
+cursorObj.execute(
+    "SELECT name FROM sqlite_master WHERE type = 'trigger' AND name = 'features_delete_log';"
+)
+if cursorObj.rowcount == 0:
+    cursorObj.execute(
+        "CREATE TRIGGER features_delete_log AFTER DELETE ON FEATURES BEGIN INSERT INTO features_logs(old_location, old_class, old_latitude, old_longitude, old_map, old_elev, user_action, created_at) VALUES(old.location, old.class, old.latitude, old.longitude, old.map, old.elev, 'DELETE', DATETIME('NOW')); END;")
+
+
+cursorObj.execute(
+    "SELECT name FROM sqlite_master WHERE type = 'trigger' AND name = 'features_update_log';"
+)
+if cursorObj.rowcount == 0:
+    cursorObj.execute(
+        "CREATE TRIGGER features_update_log AFTER UPDATE ON FEATURES WHEN old.location <> new.location OR old.class <> new.class OR old.latitude <> new.latitude OR old.longitude <> new.longitude OR old.map <> new.map OR old.elev <> new.elev BEGIN INSERT INTO sightings_logs VALUES(old.location, new.location, old.class, new.class, old.latitude, new.latitude, old.longitude, new.longitude, old.map, new.map, old.elev, new.elev, 'UPDATE', DATETIME('NOW')); END;")
+
+
+#FLOWERS
+cursorObj.execute(
+    "SELECT name FROM sqlite_master WHERE type = 'trigger' AND name = 'flowers_insert_log';"
+)
+if cursorObj.rowcount == 0:
+    cursorObj.execute(
+        "CREATE TRIGGER flowers_insert_log AFTER INSERT ON FLOWERS BEGIN INSERT INTO flowers_logs(new_genus, new_species, new_comname, user_action, created_at) VALUES(new.genus, new.species, new.comname, 'INSERT', DATETIME('NOW')); END;")
+
+
+cursorObj.execute(
+    "SELECT name FROM sqlite_master WHERE type = 'trigger' AND name = 'flowers_delete_log';"
+)
+if cursorObj.rowcount == 0:
+    cursorObj.execute(
+        "CREATE TRIGGER flowers_delete_log AFTER DELETE ON FLOWERS BEGIN INSERT INTO flowers_logs(old_genus, old_species, old_comname, user_action, created_at) VALUES(old.genus, old.species, old.comname, 'DELETE', DATETIME('NOW')); END;")
+
+
+cursorObj.execute(
+    "SELECT name FROM sqlite_master WHERE type = 'trigger' AND name = 'flowers_update_log';"
+)
+if cursorObj.rowcount == 0:
+    cursorObj.execute(
+        "CREATE TRIGGER flowers_update_log AFTER UPDATE ON FLOWERS WHEN old.genus <> new.genus OR old.species <> new.species OR old.comname <> new.comname BEGIN INSERT INTO flowers_logs VALUES(old.genus, new.genus, old.species, new.species, old.comname, new.comname, 'UPDATE', DATETIME('NOW')); END;")
+
+
 # Create trigger to keep SIGHTINGS table updated as well
 cursorObj.execute(
     "SELECT name FROM sqlite_master WHERE type = 'trigger' AND name = 'up_flowers';"
@@ -31,33 +125,16 @@ if cursorObj.rowcount == 0:
     cursorObj.execute(
         "CREATE TRIGGER up_flowers AFTER UPDATE ON Flowers BEGIN UPDATE Sightings SET name = new.comname WHERE name = old.comname; END;")
 
-# Create trigger to log changes
-# Trigger for update on flowers
-# before creating trigger, make sure it does not exist
-cursorObj.execute(
-    "SELECT name FROM sqlite_master WHERE type = 'trigger' AND name = 'log_up_flowers';"
-)
-if cursorObj.rowcount == 0:
-    cursorObj.execute(
-        "CREATE TRIGGER log_up_flowers AFTER UPDATE on FLOWERS "
-        "BEGIN"
-          "INSERT INTO log_changes VALUES(\"Update\", \"Flowers\", old.genus, old.species, "
-            "old.comname, new.genus, new.species, new.comname,"
-            " \"n/a\",\"n/a\",\"n/a\",\"n/a\"); "
-        "end; ")
-
 # close the connection to the database
 conn.close()
 
-
 # Python3 code to convert tuple into string
 def convertTuple(tup):
-    str = ''.join(tup)
+    str =  ''.join(tup)
     return str
 
-
 # This is the app route for the "Flower" Page form to receive user input
-@app.route('/select_flower', methods=['POST'])
+@app.route('/select_flower', methods = ['POST'])
 def select_flower():
     # Request the receive input (POST) to get the selected flower
     sel_Flower = request.form['sel_Flower']
@@ -68,8 +145,7 @@ def select_flower():
     # create a cursor to query the database within the app route
     cursorObj = conn.cursor()
     # Run the Query on SIGHTINGS table to get the 10 most recent sightings of the selected flower (from user input)
-    cursorObj.execute(
-        "SELECT * FROM SIGHTINGS WHERE name = \"" + sel_Flower + "\" ORDER by date(sighted) DESC limit 10")
+    cursorObj.execute("SELECT * FROM SIGHTINGS WHERE name = \"" + sel_Flower + "\" ORDER by date(sighted) DESC limit 10")
     # save the queried tuples to the "flowerMostRec"
     flowerMostRec = cursorObj.fetchall()
     # close the connection to the database
@@ -78,14 +154,17 @@ def select_flower():
     # render the next template to display the data
     return render_template('flowers_display10.html', flowerMostRec=flowerMostRec, sel_Flower=sel_Flower)
 
-
 # Update App Route
-@app.route('/update_flower', methods=['POST'])
+@app.route('/update_flower', methods = ['POST'])
 def update_flower():
     # Establish a connection with the database file
     conn = sqlite3.connect('flowers2019.db')
     # create a cursor to query the database within the app route
     cursorObj = conn.cursor()
+
+    # Create trigger to keep SIGHTINGS table updated as well
+    # cursorObj.execute(
+    #     "CREATE TRIGGER up_flowers AFTER UPDATE ON Flowers BEGIN UPDATE Sightings SET name = new.comname WHERE name = old.comname; END;")
 
     # Request the receive input (POST) to updated flower
     sel_comname = request.form['sel_Upd_Flower_Comname']
@@ -134,32 +213,6 @@ def update_flower():
     # sName = convertTuple(cursorObj.fetchone())
     # print("Sightings name updated to: '" + sName + "'")
 
-    # Check if updated attributes are blank
-    up_genus = request.form['up_genus']
-    if up_genus == '':
-        up_genus = sel_genus
-    print("The Flower's new genus is '" + up_genus + "'")
-
-    up_species = request.form['up_species']
-    if up_species == '':
-        up_species = sel_species
-    print("The Flower's new species is '" + up_species + "'")
-
-    up_comname = request.form['up_comname']
-    if up_comname == '':
-        up_comname = sel_comname
-    print("The Flower's new comname is '" + up_comname + "'")
-
-    # Run the Modification on FLOWERS table to UPDATE
-    cursorObj.execute(
-        "UPDATE FLOWERS SET genus = \"" + up_genus + "\", species = \"" + up_species + "\", comname = \"" + up_comname + "\" WHERE comname = \"" + sel_comname + "\"")
-
-    cursorObj.execute(
-        "SELECT name FROM SIGHTINGS WHERE name = \"" + up_comname + "\"")
-    print(cursorObj.fetchone())
-    # sName = convertTuple(cursorObj.fetchone())
-    # print("Sightings name updated to: '" + sName + "'")
-
     # Run the query to get the updated values
     cursorObj.execute(
         "SELECT * FROM FLOWERS WHERE COMNAME = \"" + up_comname + "\"")
@@ -171,44 +224,40 @@ def update_flower():
     conn.close()
 
     # render the next template to display the data
-    return render_template('update_flower.html', query_updated=query_updated, sel_Flower=sel_comname)
+    return render_template('update_flower.html', query_updated=query_updated)
 
 
 # Need an app route for insertion specification (instructions)
-@app.route('/insert_sighting', methods=['POST'])
-def insert_sighting():
-    # Request the receive input (POST) to new SIGHTING
-    in_fl_name = request.form['sel_InsS_Flower_Comname']
-    print("The new Sighting's Flower's name is '" + in_fl_name + "'")
+@app.route('/insert_flower', methods = ['POST'])
+def insert_flower():
+    # Request the receive input (POST) to new flower
+    in_genus = request.form['in_genus']
+    print("The new Flower's genus is '" + in_genus + "'")
 
-    in_person = request.form['in_person']
-    print("The new Sighting's Person name is '" + in_person + "'")
+    in_species = request.form['in_species']
+    print("The new Flower's species is '" + in_species + "'")
 
-    in_location = request.form['in_location']
-    print("The new Sighting's Location name is '" + in_location + "'")
-
-    in_sighted = request.form['in_sighted']
-    print("The new Sighting's Date is '" + in_sighted + "'")
+    in_comname = request.form['in_comname']
+    print("The new Flower's comname is '" + in_comname + "'")
 
     # Establish a connection with the database file
     conn = sqlite3.connect('flowers2019.db')
     # create a cursor to query the database within the app route
     cursorObj = conn.cursor()
     # Run the Modification on FLOWERS table to insert
-    cursorObj.execute("INSERT INTO SIGHTINGS VALUES (\"" + in_fl_name + "\" ,\"" + in_person + "\" ,\""
-                      + in_location + "\" ,\"" + in_sighted + "\")")
+    cursorObj.execute("INSERT INTO FLOWERS VALUES (\"" + in_genus + "\" ,\"" + in_species + "\" ,\"" + in_comname + "\")")
     # commit these changes
     conn.commit()
     # Run the query to get the insertion values
     cursorObj.execute(
-        "SELECT * FROM SIGHTINGS WHERE name = \"" + in_fl_name + "\" AND sighted = \"" + in_sighted + "\"")
+        "SELECT * FROM FLOWERS WHERE COMNAME = \"" + in_comname +"\"")
     # save the queried tuple to the "query_inserted"
     query_inserted = cursorObj.fetchall()
     # close the connection to the database
     conn.close()
 
     # render the next template to display the data
-    return render_template('insert_flower.html', query_inserted=query_inserted, sel_Flower=in_fl_name)
+    return render_template('insert_flower.html', query_inserted=query_inserted)
 
 
 # App route for the page of Flowers on the web app
@@ -227,87 +276,6 @@ def flowers():
     conn.close()
 
     return render_template('flowers.html', itemsFlowers=itemsFlowers)
-
-
-# App route to login on the web app
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-
-# App route to login on the web app
-@app.route('/login_form', methods=['POST'])
-def login_form():
-    # Request the receive input (POST) to new user
-    email = request.form['login_email']
-    print("The user's email is '" + email + "'")
-
-    password = request.form['login_pw']
-    print("The user's password is '" + password + "'")
-
-    # Establish a connection with the database file
-    conn = sqlite3.connect('flowers2019.db')
-    # create a cursor to query the database within the app route
-    cursorObj = conn.cursor()
-    # All items retrieval from FLOWERS table in DB
-    cursorObj.execute("SELECT fullName FROM USERS WHERE email = \"" + email + "\" AND password = \"" + password + "\"")
-    # save fetch all to itemsFlowers list; to be referenced in templates
-    query_tuple = cursorObj.fetchone()
-    # close the connection to the database
-    conn.close()
-
-    # if query was empty, user does not exist
-    if not (query_tuple):
-        print("User does not exist")
-        return render_template('login_failure.html')
-    else:
-        query_name = convertTuple(query_tuple)
-        return render_template('login_success.html', query_name=query_name)
-
-
-# App route to log out
-@app.route('/logout')
-def logout():
-    return render_template('logout.html')
-
-
-@app.route('/logout_form', methods=['POST'])
-def logout_form():
-    return render_template('logout_success.html')
-
-
-# App route to sign up on the web app
-@app.route('/sign_up')
-def sign_up():
-    return render_template('sign_up.html')
-
-
-@app.route('/sign_up_form', methods=['POST'])
-def sign_up_form():
-    # Request the receive input (POST) to new user
-    fullName = request.form['fullName']
-    print("The user's full name is '" + fullName + "'")
-
-    email = request.form['email']
-    print("The user's email is '" + email + "'")
-
-    password = request.form['password']
-    print("The user's password is '" + password + "'")
-
-    # Establish a connection with the database file
-    conn = sqlite3.connect('flowers2019.db')
-    # create a cursor to query the database within the app route
-    cursorObj = conn.cursor()
-    # Run the Modification on FLOWERS table to insert
-    cursorObj.execute(
-        "INSERT INTO USERS VALUES (\"" + fullName + "\" ,\"" + email + "\" ,\"" + password + "\")")
-    # commit these changes
-    conn.commit()
-    # close the connection to the database
-    conn.close()
-
-    return render_template('sign_up_complete.html')
-
 
 # App route for the Home page on the web app
 @app.route('/')
